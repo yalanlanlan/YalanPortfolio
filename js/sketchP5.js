103877394c6e0b068b943aa43d7c392df1ab8aa6
lan <leaps7bounds@gmail.com>
2019-05-05 21:40:08 -0400

    https
    https

--- js/sketchP5.js
+++ js/sketchP5.js
@@ -1,6 +1,6 @@
 
 var weather;
-var api = 'http://api.openweathermap.org/data/2.5/weather?q=';
+var api = 'https://api.openweathermap.org/data/2.5/weather?q=';
 var city='New York';
 var apiKey = '&appid=eedf376d354bb8e856c0eeb272de048b';//plz use your own api key if copy the code -yalan
 var units='&units=metric';
