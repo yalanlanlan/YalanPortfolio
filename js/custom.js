c1eb9125cbdc0c362247166733355635a30fb565
yalanlanlan <yalanlanlan@users.noreply.github.com>
2020-10-06 23:58:14 -0400

    preloadText
    420px

--- js/custom.js
+++ js/custom.js
@@ -2,7 +2,7 @@
  /* jQuery Pre loader
   -----------------------------------------------*/
 $(window).load(function(){
-    $('.preloader').fadeOut(3000); // set duration in brackets    
+    $('.preloader').fadeOut(1000); // set duration in brackets    
 });
 
 
